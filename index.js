8f14f10d1fcdc83083b4c2b7aef98512d3076cf2
nourFarid <norfared13@gmail.com>
2023-12-11 03:35:08 +0200

    test

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000
 initApp(app ,express)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port  ${port}!`))
+
+
