d346ccea7372fb5f270541fe1d05550d117a8796
nourhan <norfared13@gmail.com>
2024-01-27 14:08:44 +0200

    timing update

--- DB/model/timing/timingOldFemales.js
+++ DB/model/timing/timingOldFemales.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const timingOldFemalesSchema = new mongoose.Schema({
+  ofYear:{
+    type:String,
+    
+  },
+
   to: {
     type: String,
     required: [true, "String is required"],
